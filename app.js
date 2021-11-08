7bb3545a809d6e9184511334d247c4c043e4ceb6
brenda <lamasbrenda@hotmail.com>
2021-11-07 21:08:15 -0300

    ejercicios Metodos de Array

---
+++ app.js
@@ -0,0 +1,259 @@
+// 1
+// Tenemos un array en una variable numeros con números al azar.
+// Usá la función map para crear un nuevo array incrementando cada valor del array en 10, y guardarlo en la variable numerosMasDiez
+// Mostrar por consola el array original y el nuevo
+const numeros = [1, 2, 3, 4, 5];
+// codear acá la solución del ejercicio
+
+const numeroMasDiez = numeros.map((numero)=>{
+    const sumando = (numero+10)
+    return sumando
+})
+console.log(numeros); // [1, 2, 3, 4, 5]
+console.log(numerosMasDiez); // [11, 12, 13, 14, 15]
+
+// 2
+// Tenemos un array en una variable numeros con números al azar.
+// Usar la función map para crear un nuevo array multiplicando cada valor del array por 2, y guardarlo en la variable dobles
+// Mostrar por consola el array original y el nuevo
+const numerosAlAzar = [3, 7, 13, 99];
+// // codear acá la solución del ejercicio
+
+const dobles = numerosAlAzar.map((numero)=>{
+    const multiplicacion = numero*2
+    return multiplicacion
+})
+
+console.log(numerosAlAzar); // [3, 7, 13, 99]
+console.log(dobles); // [6, 14, 26, 198]
+
+// 3
+// Tenemos un array en una variable frases con frases al azar.
+// Usar la función map para crear un nuevo array donde cada frase empiece y termine con signo de exclamación.
+// Mostrar por consola el array original y el nuevo
+// const frases = ['Labore sea dolor.', 'Justo rebum dolor.', 'Stet lorem amet.'];
+
+// codear acá la solución del ejercicio
+
+const frasesExclamadas = frases.map((frase)=>{
+    return `¡${frase}!`
+})
+
+console.log(frases); // ['Labore sea dolor.', 'Justo rebum dolor.', 'Stet lorem amet.']
+console.log(frasesExclamadas); // [ '¡Labore sea dolor.!', '¡Justo rebum dolor.!', '¡Stet lorem amet.!' ]
+
+// 4
+// Tenemos un array en una variable libros con libros para leer sobre Javascript.
+// Usar la función map para crear un nuevo array donde cada titulo de los libros esté encerrado en una etiqueta <li></li>.
+// Mostrar por consola el array nuevo, encerrándolo entre <ul></ul>
+const librosDeJS = [
+  'JavaScript for Kids: A Playful Introduction to Programming',
+  'Composing Software',
+  'Eloquent JavaScript: A Modern Introduction to Programming',
+  'JavaScript: The Good Parts',
+  'Programming JavaScript Applications: Robust Web Architecture with Node, HTML5, and Moderns JS Libraries',
+  'Effective JavaScript: 68 Specific Ways to Harness the Power of JavaScript',
+  'JavaScript: The Definitive Guide',
+  'You Don’t Know JS',
+  'JavaScript Allongé: The Six Edition'
+];
+
+// codear acá la solución del ejercicio
+
+const nuevoArray = librosDeJS.map((libro)=>{
+    return `<li>${libro}</li>`
+})
+
+console.log(`<ul> ${nuevoArray.join("")} </ul>`);
+
+// el resultado final debería ser
+// <ul><li>JavaScript for Kids: A Playful Introduction to Programming</li><li>Composing Software</li><li>Eloquent JavaScript: A Modern Introduction to Programming</li><li>JavaScript: The Good Parts</li><li>Programming JavaScript Applications: Robust Web Architecture with Node, HTML5, and Moderns JS Libraries</li><li>Effective JavaScript: 68 Specific Ways to Harness the Power of JavaScript</li><li>JavaScript: The Definitive Guide</li><li>You Don’t Know JS</li><li>JavaScript Allongé: The Six Edition</li></ul>
+
+
+// 5
+// Tenemos un array en una variable frases con frases al azar.
+// Usar la función map para crear un nuevo array que contenga la longitud de cada frase.
+// Mostrar por consola el array original y el nuevo.
+const frases = ['Labore sea dolor.', 'Justo rebum dolor.', 'Stet lorem amet.'];
+
+// codear acá la solución del ejercicio
+
+const longitudes = frases.map((frase, index, array)=>{
+    return frase.length
+})
+
+console.log(frases); // ['Labore sea dolor.', 'Justo rebum dolor.', 'Stet lorem amet.']
+console.log(longitudes); // [ 17, 18, 16 ]
+
+// 6
+// Tenemos un array en una variable playlist con una lista de canciones de un disco.
+// Usar la función map para agregar a cada título de la canción el número de posición en la que está dentro del array.
+// const playlist = ['Everlong', 'The Pretender', 'Learn to Fly'];
+
+// completá acá el código
+
+const playlistConIndex = playlist.map((play, index, array)=>{
+    return `${index} - ${play}`
+})
+
+console.log(playlistConIndex);
+// RESULTADO ESPERADO
+// [ '0 - Everlong', '1 - The Pretender', '2 - Learn to Fly' ]
+
+// 7
+// Tenemos un array en una variable costos con números que representan costos de diferentes productos.
+// Completar el siguiente código para llegar al resultado esperado
+// A un costo primero se le agrega la ganancia y después el IVA (este ultimo sobre el costo + la ganancia)
+const costos = [ 12.5, 56, 98, 45.75 ];
+
+const agregarIVA = (costo) => {
+  return costo * 1.21;
+}
+
+const sumarGanancia = (costo) => {
+  return costo * 1.5;
+}
+
+// codear acá la solución del ejercicio
+const preciosFinales = costos.map((costo)=>{
+    const resultadoParcial = sumarGanancia(costo)
+    const resultadoTOTAL = agregarIVA(resultadoParcial)
+    return resultadoTOTAL
+})
+
+console.log(preciosFinales);
+// deberia mostrar
+// [ 22.6875, 101.64, 177.87, 83.03625 ]
+
+
+// 8
+// Tenemos un array en una variable costos con números que representan costos de diferentes productos.
+// También tenemos un array en una variable productos con los nombres de cada producto.
+// Completar el siguiente código para llegar al resultado esperado
+// A un costo primero se le agrega la ganancia y después el IVA
+// El nombre de un producto en una posición, se corresponde con el precio que está en la misma posición. 
+// Por ejemplo: el producto que está en la posición 1 tiene un costo igual al elemento en la posición 1 del array costos
+
+const productos = [ 'celular', 'notebook', 'monitor' ];
+const costosSegunProducto = [ 12.5, 56, 98 ];
+
+// const agregarIVA = (costo) => {
+//   return costo * 1.21;
+// }
+
+// const sumarGanancia = (costo) => {
+//   return costo * 1.5;
+// }
+
+// codear acá la solución del ejercicio
+
+const preciosFinalesCostoYProducto = costosSegunProducto.map((costo)=>{
+    const resultadoPARCIAL = sumarGanancia(costo)
+    const resultadoFINAL = agregarIVA(resultadoPARCIAL)
+    return `${productos[0]} ${resultadoFINAL}`
+})
+
+console.log(preciosFinalesCostoYProducto);
+// deberia mostrar
+// [ "celular 22.6875", "notebook 101.64", "monitor 177.87" ]
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Filter
+// 1
+// Tenemos un array en una variable costos con números que representan costos de diferentes productos.
+// Usando filter, crear un nuevo array con los precios más caros (mayores a 50) y guardarlo en la variable losMasCaros
+// Mostrar el array original y el filtrado
+const costosFilter = [ 39, 53, 17, 99, 7, 9, 6, 68, 54, 97, 27, 90, 92, 75, 26, 86, 22, 42, 20, 14 ];
+// codear acá la solución del ejercicio
+const losMasCaros = costosFilter.filter((costo)=>{
+    return costo > 50
+})
+
+console.log(costosFilter);
+console.log(losMasCaros);
+// deberia mostrar
+// [ 39, 53, 17, 99, 7, 9, 6, 68, 54, 97, 27, 90, 92, 75, 26, 86, 22, 42, 20, 14 ]
+// [ 53, 99, 68, 54, 97, 90, 92, 75, 86 ]
+
+
+// 2
+// Tenemos un array en una variable numeros con números al azar.
+// También tenemos dos arrays vacios en las dos variables numerosPares y numerosImpares.
+// Utilizando filter crear un nuevo array con todos los números pares y guardalo en la variable numerosPares
+// Utilizando filter crear un nuevo array con todos los números impares y guardalo en la variable numerosImpares
+const masNumeros = [ 43, 11, 18, 46, 44, 37, 42, 29, 9, 3, 37, 0, 40, 10, 38, 34, 25, 40, 4, 32 ];
+
+// codea acá tu solución
+const numerosPares = []
+const numerosImpares = []
+const buscandoParOImpar = masNumeros.filter((elemento,index, array)=>{
+    if(elemento % 2 === 0){
+        numerosPares.push(elemento)
+    }else{
+        numerosImpares.push((elemento))
+    }
+})
+
+console.log("pares: ", numerosPares);
+console.log("impares: ", numerosImpares);
+// deberias tener como resultado
+// pares: [18, 46, 44, 42, 0, 40, 10, 38, 34, 40, 4, 32]
+// impares: [43, 11, 37, 29, 9, 3, 37, 25]
+
+
+
+// 3
+// Tenemos un array en una variable mix con varios elementos, de distintos tipos de datos.
+// Usando filter, crear un nuevo array con todos los elementos que sean strings y guardalo en la variable soloStrings.
+// Ayuda: para saber si algo es un string en javascript, podés usar typeof 
+// Mostrar el array resultante
+const mix = [
+  'Ut vero.',
+  2,
+  function () { console.log('hola mundo!') },
+  56,
+  'Diam rebum nonumy et.',
+  true,
+  false,
+  'Kasd stet.',
+  'Sit et dolor.',
+  null,
+  null,
+  [ 1, 2, 3],
+  'Dolore.'
+];
+
+// codear acá la solución del ejercicio
+const soloStrings = mix.filter ((elemento, index, array)=>{
+    return typeof elemento === "string"
+    
+})
+
+console.log(soloStrings);
+// deberia mostrar
+// [ 'Ut vero.', 'Diam rebum nonumy et.', 'Kasd stet.', 'Sit et dolor.', 'Dolore.' ]
+
+
+// 4
+// Tenemos un array playlist con canciones seleccionadas al azar por spotify para reproducir.
+// Tenemos otro array playlistEscuchada que tiene canciones que ya escuchamos anteriormente.
+// Usando filter, queremos crear una nueva lista que solo contenga aquellas canciones guardadas en playlist que no están en playlistEscuchada
+// Guarda el resultado en la variable playlistSinEscuchar
+const playlistFilter = ['Smells Like Teen Spirit', 'Everlong', 'Come As You Are', 'The Pretender', 'Heart-Shaped Box', 'Learn to Fly', 'Lithium'];
+var playlistEscuchada = ['The Pretender', 'Lithium', 'Come As You Are']
+
+// codea aca tu solución
+const playlistSinEscuchar = playlistFilter.filter((elemento, index, array)=>{
+    
+    return playlist.includes(`${playlistEscuchada}`)
+})
+
+console.log(playlistFilter);
+console.log(playlistEscuchada);
+console.log(playlistSinEscuchar);
+
+// deberia mostrar
+// [ 'Smells Like Teen Spirit', 'Everlong', 'Heart-Shaped Box', 'Learn to Fly' ]
